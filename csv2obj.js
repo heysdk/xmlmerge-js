5321902d982b302086ebe2d2c556ffb8ac97a8e8
heysdk <51278370@qq.com>
2015-01-08 10:49:19 +0800

    0.2.0
    1. add proj to github
    
    zhs007 @ 2015.01.08

---
+++ csv2obj.js
@@ -0,0 +1,36 @@
+/**
+ * Created by zhs007 on 2014/12/3.
+ */
+
+function csv2obj(str) {
+    var obj = [];
+    var head = [];
+
+    if (typeof (str) == 'undefined' || str.length <= 0) {
+        return obj;
+    }
+
+    //var str = csvinfo.toString();
+
+    var lstLine = str.split('\r\n');
+    var lines = lstLine.length;
+    if (lines > 0) {
+        var lstHead = lstLine[0].split(',');
+        var numsHead = lstHead.length;
+
+        for (var i = 1; i < lines; ++i) {
+            var val = lstLine[i].split(',');
+            var nums = val.length;
+            if (nums == numsHead) {
+                obj[i - 1] = {};
+                for(var j = 0; j < numsHead; ++j) {
+                    obj[i - 1][lstHead[j]] = val[j];
+                }
+            }
+        }
+    }
+
+    return obj;
+}
+
+exports.csv2obj = csv2obj;
